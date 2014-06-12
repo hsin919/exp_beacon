ec78715a331e328e457b716d9742bede609748fc
Nathan <hsin919@gmail.com>
2014-06-12 15:33:58 +0800

    Init import

---
+++ Beacon/ViewController.m
@@ -0,0 +1,77 @@
+//
+//  ViewController.m
+//  Beacon
+//
+//  Created by Christopher Ching on 2013-11-28.
+//  Copyright (c) 2013 AppCoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create a NSUUID object
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"];
+    
+    time_t t;
+    srand((unsigned) time(&t));
+    // Initialize the Beacon Region
+    self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                  major:rand()
+                                                                  minor:rand()
+                                                             identifier:@"SomeIdentifier"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonClicked:(id)sender {
+    
+    // Get the beacon data to advertise
+    self.myBeaconData = [self.myBeaconRegion peripheralDataWithMeasuredPower:nil];
+    
+    // Start the peripheral manager
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+}
+
+-(void)peripheralManagerDidUpdateState:(CBPeripheralManager*)peripheral
+{
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn)
+    {
+        // Bluetooth is on
+        
+        // Update our status label
+        self.statusLabel.text = @"Broadcasting...";
+        
+        // Start broadcasting
+        [self.peripheralManager startAdvertising:self.myBeaconData];
+    }
+    else if (peripheral.state == CBPeripheralManagerStatePoweredOff)
+    {
+        // Update our status label
+        self.statusLabel.text = @"Stopped";
+        
+        // Bluetooth isn't on. Stop broadcasting
+        [self.peripheralManager stopAdvertising];
+    }
+    else if (peripheral.state == CBPeripheralManagerStateUnsupported)
+    {
+        self.statusLabel.text = @"Unsupported";
+    }
+}
+
+@end
