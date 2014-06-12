47c480a31dc0a356c7e6aa3351f6f44f9de4ebb3
Nathan <hsin919@gmail.com>
2014-06-12 17:22:48 +0800

    Add service & characteristic.

--- Beacon/ViewController.m
+++ Beacon/ViewController.m
@@ -8,8 +8,15 @@
 
 #import "ViewController.h"
 
+static NSString * const kUUID = @"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0";
+static NSString * const kServiceUUID = @"E28E86A2-45A2-4E39-B0F0-045446794698";
+static NSString * const kCharacteristicUUID = @"4FBAF52F-925F-4958-86EF-68984BEFB5C7";
+
 @interface ViewController ()
 
+@property (nonatomic, strong) CBMutableCharacteristic *customCharacteristic;
+@property (nonatomic, strong) CBMutableService *customService;
+@property (nonatomic, strong) NSMutableArray *centralArray;
 @end
 
 @implementation ViewController
@@ -20,7 +27,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     // Create a NSUUID object
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"];
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:kUUID];
     
     time_t t;
     srand((unsigned) time(&t));
@@ -29,23 +36,65 @@
                                                                   major:rand()
                                                                   minor:rand()
                                                              identifier:@"SomeIdentifier"];
+    
+    self.centralArray = [[NSMutableArray alloc] init];
+    
+    self.peripheralManager = nil;
 }
 
+- (void)setupService
+{
+    CBUUID *characteristicUUID = [CBUUID UUIDWithString:kCharacteristicUUID];
+    self.customCharacteristic = [[CBMutableCharacteristic alloc] initWithType:characteristicUUID properties:CBCharacteristicPropertyNotify value:nil permissions:CBAttributePermissionsReadable];
+    
+    CBUUID *serviceUUID = [CBUUID UUIDWithString:kServiceUUID];
+    self.customService = [[CBMutableService alloc] initWithType:serviceUUID primary:YES];
+    
+    [self.customService setCharacteristics:@[self.customCharacteristic]];
+    [self.peripheralManager addService:self.customService];
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
 - (IBAction)buttonClicked:(id)sender {
     
-    // Get the beacon data to advertise
-    self.myBeaconData = [self.myBeaconRegion peripheralDataWithMeasuredPower:nil];
+    if(self.peripheralManager == nil)
+    {
+        // Get the beacon data to advertise
+        self.myBeaconData = [self.myBeaconRegion peripheralDataWithMeasuredPower:nil];
+        
+        // Start the peripheral manager
+        self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                         queue:nil
+                                                                       options:nil];
+    }
+    else
+    {
+        if([self.centralArray count]>0)
+        {
+            NSLog(@"Update value to %@", self.centralArray);
+            [self.peripheralManager updateValue:nil forCharacteristic:self.customCharacteristic onSubscribedCentrals:self.centralArray];
+        }
+    }
     
-    // Start the peripheral manager
-    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
-                                                                     queue:nil
-                                                                   options:nil];
+}
+
+#pragma mark - Beacon advertising delegate methods
+- (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheralManager error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"Couldn't turn on advertising: %@", error);
+        return;
+    }
+    
+    if (peripheralManager.isAdvertising) {
+        NSLog(@"Turned on advertising.");
+    }
 }
 
 -(void)peripheralManagerDidUpdateState:(CBPeripheralManager*)peripheral
@@ -58,7 +107,8 @@
         self.statusLabel.text = @"Broadcasting...";
         
         // Start broadcasting
-        [self.peripheralManager startAdvertising:self.myBeaconData];
+        //[self.peripheralManager startAdvertising:self.myBeaconData];
+        [self setupService];
     }
     else if (peripheral.state == CBPeripheralManagerStatePoweredOff)
     {
@@ -71,6 +121,29 @@
     else if (peripheral.state == CBPeripheralManagerStateUnsupported)
     {
         self.statusLabel.text = @"Unsupported";
+    }
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(NSError *)error
+{
+    NSLog(@"%s", __FUNCTION__);
+    if(error == nil)
+    {
+        NSLog(@"add service success");
+        
+        [self.peripheralManager startAdvertising:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                  @"ICServer", CBAdvertisementDataLocalNameKey,
+                                                  @[[CBUUID UUIDWithString:kServiceUUID]], CBAdvertisementDataServiceUUIDsKey,
+                                                  nil]];
+    }
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
+{
+    NSLog(@"%s", __FUNCTION__);
+    if(![self.centralArray containsObject:central])
+    {
+        [self.centralArray addObject:central];
     }
 }
 
